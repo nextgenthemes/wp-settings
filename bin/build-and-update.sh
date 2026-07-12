0ddec899b3d84818a3b2d40d69c51a2d30672334
Nicolas Jonas <nnico+git@pm.me>
2026-07-13 01:44:58 +0200

    Add @wordpress/interactivity dependency and update build script

--- bin/build-and-update.sh
+++ bin/build-and-update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -Eeuox pipefail
 
 cd "$(dirname "$0")/.."
 
 deno task build
 
+git add build/
+git commit -m "build" || true
+
+opencode run --model opencode/big-pickle 'commit package'
